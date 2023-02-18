f4151e2c96552372ae1939ab2d59e3628df8dcc9
diplomat <diplomatgmg@gmail.com>
2023-02-18 22:22:35 +0300

    Добавлен help text в форму профиля

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,12 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
 
 class UserLoginForm(AuthenticationForm):
-    username_or_email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form__input'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
+    username_or_email = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
 
     class Meta:
         model = CustomUser
@@ -14,10 +18,18 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form__input'}))
-    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form__input'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form__input'
+        }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
 
     class Meta:
         model = CustomUser
@@ -25,20 +37,45 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    username = forms.CharField(disabled=True, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    about_me = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input',
-                                                                             'placeholder': 'Напишите пару слов о себе...'}))
+    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    username = forms.CharField(disabled=True, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    about_me = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input',
+        'placeholder': 'Напишите пару слов о себе...'
+        }))
     image = forms.ImageField(required=False)
-    email = forms.CharField(required=False, widget=forms.EmailInput(attrs={'class': 'form__input'}))
-    phone = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
+    email = forms.CharField(required=False, widget=forms.EmailInput(attrs={
+        'class': 'form__input'
+        }))
+    phone = forms.RegexField(required=False,
+                             regex=r'^[+]?\d{10,11}$',
+                             help_text='Формат: 7 999 999 99 99',
+                             widget=forms.TextInput(attrs={
+                                 'class': 'form__input'
+                                 }))
 
-    street = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    city = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    house = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    building = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
-    apartment = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form__input'}))
+    street = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    city = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    house = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    building = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
+    apartment = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'class': 'form__input'
+        }))
 
     class Meta:
         model = CustomUser
@@ -48,9 +85,15 @@ class UserProfileForm(UserChangeForm):
 
 
 class UserChangePasswordForm(PasswordChangeForm):
-    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
-    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
-    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form__input'}))
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form__input'
+        }))
 
     class Meta:
         model = CustomUser
