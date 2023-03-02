a724c5af4f1dbd5fb97150adbff3dc94dcff3a44
diplomat <diplomatgmg@gmail.com>
2023-03-02 17:48:26 +0300

    Добавлены тесты моделей

--- apps/index/tests.py
+++ apps/index/tests.py
@@ -1,7 +1,5 @@
 from http import HTTPStatus
-from http.client import HTTPResponse
 
-from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import reverse
 
@@ -9,53 +7,54 @@ from apps.products.models import Product, ProductCategory, Brand
 
 
 class IndexTestCase(TestCase):
+    fixtures = ['fixtures/brands.json', 'fixtures/categories.json', 'fixtures/products.json']
+
     def setUp(self) -> None:
-        ProductCategory.objects.create(category='Смартфоны')
-        Brand.objects.create(name='Google')
+        self.brands = Brand.objects.all()
+        self.product_categories = ProductCategory.objects.all()
+        self.products = Product.objects.all()
 
-        self.brand = Brand.objects.last()
-        self.product_category = ProductCategory.objects.last()
-
-        Product.objects.create(
-            brand=self.brand,
-            category=self.product_category,
-            name='Pixel 6a',
-            description='Описание смартфона',
-            real_price=10000,
-            discount=10,
-            rating=4.5,
-            quantity=5,
-            count_sales=10, )
-
-        self.product = Product.objects.last()
-
-    def test_html(self):
+    def test_index(self):
         index_url = reverse('index:index')
         response = self.client.get(index_url)
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
-        self.assertEqual(response.template_name, ['index/index.html'])
+        self.assertTemplateUsed(response, 'index/index.html')
+
+        best_rating_product = self.products.order_by('-rating').first()
+        self.assertEqual(response.context_data['best_rating_product'], best_rating_product)
+
+        best_price_products = self.products.order_by('price', '-rating')[:8]
+        self.assertEqual(len(response.context_data['best_price_products']), 8)
+        self.assertQuerysetEqual(response.context_data['best_price_products'], best_price_products)
+
+        best_rating_products = self.products.order_by('-rating', 'price')[:8]
+        self.assertEqual(len(response.context_data['best_rating_products']), 8)
+        self.assertQuerysetEqual(response.context_data['best_rating_products'], best_rating_products)
+
+        best_sellers_products = self.products.order_by('-count_sales', 'price')[:4]
+        self.assertEqual(len(response.context_data['best_sellers_products']), 4)
+        self.assertQuerysetEqual(response.context_data['best_sellers_products'], best_sellers_products)
 
     def test_category(self):
-        self.assertEqual(str(self.product_category), 'Смартфоны')
-        self.assertEqual(self.product_category.category, 'Смартфоны')
+        self.assertEqual(str(self.product_categories.get(category='Смартфоны')), 'Смартфоны')
         self.assertEqual(len(ProductCategory.objects.all()), 1)
 
     def test_brand(self):
-        self.assertEqual(str(self.brand), 'Google')
-        self.assertEqual(self.brand.name, 'Google')
-        self.assertEqual(len(Brand.objects.all()), 1)
+        self.assertEqual(str(self.brands.get(name='Google')), 'Google')
+        self.assertEqual(len(self.brands), 4)
 
     def test_product(self):
-        self.assertEqual(self.product.name, 'Pixel 6a')
-        self.assertEqual(len(Product.objects.all()), 1)
+        self.assertEqual(len(self.products), 26)
+        product = self.products.get(name='iPhone 13 mini')
+        self.assertEqual(product.name, 'iPhone 13 mini')
 
-        discount_price = self.product.real_price - (self.product.real_price // 100 * self.product.discount)
-        self.assertEqual(self.product.real_price, 10000)
-        self.assertEqual(self.product.discount, 10)
-        self.assertEqual(self.product.price, discount_price)
+        discount_price = product.real_price - (product.real_price // 100 * product.discount)
+        self.assertEqual(product.real_price, 49000)
+        self.assertEqual(product.discount, 47)
+        self.assertEqual(product.price, discount_price)
 
-        full_name = 'Google Pixel 6a'
-        self.assertEqual(self.product.get_full_name(), full_name)
-        self.assertEqual(str(self.product), full_name)
+        full_name = 'Apple iPhone 13 mini'
+        self.assertEqual(product.get_full_name(), full_name)
+        self.assertEqual(str(product), full_name)
